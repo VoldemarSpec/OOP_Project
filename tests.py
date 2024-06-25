a70724bec2e94cf3afa2129e129e8485e121337b
Voldemar <volodymirspecialnyi@gmail.com>
2024-06-25 04:36:13 +0200

    setup added

--- tests.py
+++ tests.py
@@ -1,3 +1,11 @@
 import unittest
 from collections import Counter
 from Plants import Field, Flower, Tree, Vegetable
+
+class TestField(unittest.TestCase):
+
+    def setUp(self):
+        self.field = Field("Test Field")
+        self.rose = Flower("Rose", "Red", 10.0, 0.5, "Spring")
+        self.apple_tree = Tree("Apple Tree", "Green", 50.0, 3.0, True)
+        self.carrot = Vegetable("Carrot", "Orange", 2.0, 0.2, True)
