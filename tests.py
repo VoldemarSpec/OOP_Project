6bb4d8f44051b710099dd1fbb4c13a085a802be4
Voldemar <volodymirspecialnyi@gmail.com>
2024-06-25 04:43:18 +0200

    field and plant tests added

--- tests.py
+++ tests.py
@@ -9,3 +9,17 @@ class TestField(unittest.TestCase):
         self.rose = Flower("Rose", "Red", 10.0, 0.5, "Spring")
         self.apple_tree = Tree("Apple Tree", "Green", 50.0, 3.0, True)
         self.carrot = Vegetable("Carrot", "Orange", 2.0, 0.2, True)
+
+    def test_initial_field(self):
+        self.assertEqual(self.field.name, "Test Field")
+        self.assertEqual(self.field.get_profit(), 0)
+        self.assertEqual(len(self.field.get_plants_counter()), 0)
+
+    def test_plant(self):
+        self.field.plant(self.rose)
+        self.assertEqual(self.field.get_profit(), -10.0)
+        self.assertEqual(self.field.get_plants_counter()[self.rose], 1)
+
+        self.field.plant(self.apple_tree)
+        self.assertEqual(self.field.get_profit(), -60.0)
+        self.assertEqual(self.field.get_plants_counter()[self.apple_tree], 1)
